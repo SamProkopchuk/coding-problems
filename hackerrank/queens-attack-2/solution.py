f4b6affe336cf28e26a534eb39cc5a70a7e7372e
SamProkopchuk <ssprokopchuk@gmail.com>
2020-11-18 00:19:29 -0800

    Add final solution.py

--- hackerrank/queens-attack-2/solution.py
+++ hackerrank/queens-attack-2/solution.py
@@ -5,24 +5,22 @@ queen_pos = list(map(int, input().strip().split()))
 
 # Rip PEP8 lmao
 closest8 = [
-    [min(queen_pos[0], queen_pos[1]) - 1,        queen_pos[0] - 1,    min(queen_pos[0] - 1, size - queen_pos[0])],
+    [min(queen_pos[0], queen_pos[1]) - 1,        queen_pos[0] - 1,    min(queen_pos[0] - 1, size - queen_pos[1])],
     [queen_pos[1] - 1,                           0,                   size - queen_pos[1]],
-    [min(size - queen_pos[0], queen_pos[1] - 1), size - queen_pos[0], min(size - queen_pos[0], size - queen_pos[0])]]
+    [min(size - queen_pos[0], queen_pos[1] - 1), size - queen_pos[0], min(size - queen_pos[0], size - queen_pos[1])]]
 
-def closest8_coord(x_delta, y_delta):
-    row = -1 if x_delta > 0 else int(bool(x))
-    col = -1 if y_delta > 0 else int(bool(y))
-    row += 1
-    col += 1
+def closest8_index(board_row_delta, board_col_delta):
+    row = 1 + (-1 if board_row_delta < 0 else int(bool(row_delta)))
+    col = 1 + (-1 if board_col_delta < 0 else int(bool(col_delta)))
     return row, col
 
 for _ in range(obstacles):
     obstacle = list(map(int, input().strip().split()))
-    x = obstacle[0] - queen_pos[0]
-    y = obstacle[1] - queen_pos[1]
-    if abs(x) == abs(y):
-        # Then it is in queens line of fire
-        row, col = closest8_coord(x, y)
-        closest8[row][col] = min(closest8[row][col], abs(x)-1)
+    row_delta = obstacle[0] - queen_pos[0]
+    col_delta = obstacle[1] - queen_pos[1]
+    if abs(row_delta) == abs(col_delta) or 0 in (row_delta, col_delta):
+        # Then it is in queen's line of fire
+        row, col = closest8_index(row_delta, col_delta)
+        closest8[row][col] = min(closest8[row][col], max(abs(row_delta), abs(col_delta))-1)
 
 print(sum(chain(*closest8)))

92403ef323d0d723697af558936dd162ef07a61e
SamProkopchuk <ssprokopchuk@gmail.com>
2020-11-19 23:37:50 -0800

    Add 'encryption' solution

---
+++ hackerrank/encryption/solution.py
@@ -0,0 +1,13 @@
+from math import floor, ceil, sqrt
+
+s = input().strip().replace(" ", "")
+n = sqrt(len(s))
+rows = floor(n)
+cols = ceil(n)
+res = [''] * cols
+for col in range(cols):
+    row_vec = s[col * cols: (col + 1) * cols]
+    for idx in range(len(row_vec)):
+        res[idx] += row_vec[idx]
+
+print(" ".join(res))

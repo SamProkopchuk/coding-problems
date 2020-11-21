d8d0de87ce22193bae393d5e2dc21e5ebd0d8e72
SamProkopchuk <ssprokopchuk@gmail.com>
2020-11-21 00:04:48 -0800

    Add better climbing-the-leaderboard solution

--- hackerrank/climbing-the-leaderboard/solution.py
+++ hackerrank/climbing-the-leaderboard/solution.py
@@ -1,23 +1,9 @@
-from collections import deque
-
 input()
-scores = list(dict.fromkeys(map(int, input().strip().split())))
+# Get items without duplicates, keeping order:
+scores = [*dict.fromkeys(map(int, input().strip().split()))]
 input()
-pscores = deque(map(int, input().strip().split()))
-
-rank = len(scores)
-res = []
-
-score = scores.pop()
-while pscores:
-    pscore = pscores.popleft()
-    while scores:
-        if pscore <= score:
-            res.append(rank + (1 if pscore < score else 0))
-            break
-        score = scores.pop()
-        rank -= 1
-    else:
-        res.append(rank + (1 if pscore < score else 0))
-
-print(*res, sep = '\n')
+alice_scores = list(map(int, input().strip().split()))
+for alice_score in alice_scores:
+    while scores and scores[-1] <= alice_score:
+        del scores[-1]
+    print(len(scores)+1)

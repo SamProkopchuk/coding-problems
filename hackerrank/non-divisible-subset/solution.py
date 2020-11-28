998a53bbbe6f1f17dfe9cd77ca5fdae0e9596141
SamProkopchuk <ssprokopchuk@gmail.com>
2020-11-27 23:46:59 -0800

    Make less verbose

--- hackerrank/non-divisible-subset/solution.py
+++ hackerrank/non-divisible-subset/solution.py
@@ -1,6 +1,6 @@
 from collections import Counter
 _, rem = map(int, input().strip().split())
 remcount = Counter(i % rem for i in map(int, input().strip().split()))
-res = min(remcount[0], 1) + (rem % 2 == 0 and min(remcount[rem // 2], 1))
+res = bool(remcount[0]) + (rem % 2 == 0 and bool(remcount[rem // 2]))
 res += sum(max(remcount[c], remcount[rem-c]) for c in range(1, (rem+1) // 2))
 print(res)

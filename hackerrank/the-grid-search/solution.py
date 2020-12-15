8941638b6a8b945a5607e06f2654509555eddf63
SamProkopchuk <ssprokopchuk@gmail.com>
2020-12-15 02:36:50 -0800

    Add the-grid-search solution

---
+++ hackerrank/the-grid-search/solution.py
@@ -0,0 +1,21 @@
+from itertools import product
+
+
+def match(matrix, pattern, r, c):
+    for ridx, r_ in enumerate(matrix[r: r + len(pattern)]):
+        if r_[c: c + len(pattern[0])] != pattern[ridx]:
+            return False
+    return True
+
+
+for _ in range(int(input())):
+    mr, mc = map(int, input().split())
+    matrix = [[int(c) for c in input().strip()] for _ in range(mr)]
+    pr, pc = map(int, input().split())
+    patten = [[int(c) for c in input().strip()] for _ in range(pr)]
+    for r, c in product(range(mr - pr + 1), range(mc - pc + 1)):
+        if [r_[c: c + pc] for r_ in matrix[r: r + pr]] == patten:
+            print('YES')
+            break
+    else:
+        print('NO')

177ea97aadf752bdb9c004e69c24edf41656367a
SamProkopchuk <ssprokopchuk@gmail.com>
2020-12-19 23:53:20 -0800

    Clean up code some more

--- hackerrank/two-pluses/solution.py
+++ hackerrank/two-pluses/solution.py
@@ -1,5 +1,4 @@
 from copy import deepcopy
-from itertools import chain
 from itertools import product
 
 
@@ -9,22 +8,24 @@ def stoa(size):
     return 4 * size - 3
 
 
+def valid_idx(matrix, row, col):
+    return 0 <= row < len(matrix) and 0 <= col < len(matrix[row])
+
+
 def largest_plus(matrix, row, col, roff=0, coff=0, dr=0, dc=0):
     if matrix[row][col] == 0:
         return 0
-    elif dr:
-        if not (0 <= row+roff < len(matrix)) or matrix[row+roff][col+coff] == 0:
-            return abs(roff)
-        return largest_plus(matrix, row, col, roff + dr, coff, dr, dc)
-    elif dc:
-        if not (0 <= col+coff < len(matrix[0])) or matrix[row+roff][col+coff] == 0:
-            return abs(coff)
-        return largest_plus(matrix, row, col, roff, coff + dc, dr, dc)
-    directions = set()
-    for delta in (-1, 1):
-        directions.add(largest_plus(matrix, row, col, delta, dc, delta, dc))
-        directions.add(largest_plus(matrix, row, col, dr, delta, dr, delta))
-    return min(directions)
+    elif dr or dc:
+        if not valid_idx(matrix, row + roff, col +
+                         coff) or matrix[row + roff][col + coff] == 0:
+            return abs(roff + coff)  # One of them should be zero.
+        return largest_plus(matrix, row, col, roff + dr, coff + dc, dr, dc)
+    else:
+        directions = set()
+        for delta in (-1, 1):
+            directions.add(largest_plus(matrix, row, col, delta, 0, delta, 0))
+            directions.add(largest_plus(matrix, row, col, 0, delta, 0, delta))
+        return min(directions)
 
 
 def zero_out(matrix, row, col, dist, dr=0, dc=0):
@@ -33,11 +34,11 @@ def zero_out(matrix, row, col, dist, dr=0, dc=0):
     elif dr == dc == 0:
         matrix[row][col] = 0
         for delta in (-1, 1):
-            zero_out(matrix, row + delta, col, dist-1, delta, 0)
-            zero_out(matrix, row, col + delta, dist-1, 0, delta)
+            zero_out(matrix, row + delta, col, dist - 1, delta, 0)
+            zero_out(matrix, row, col + delta, dist - 1, 0, delta)
     else:
         matrix[row][col] = 0
-        zero_out(matrix, row+dr, col+dc, dist-1, dr, dc)
+        zero_out(matrix, row + dr, col + dc, dist - 1, dr, dc)
 
 
 def largest_bybase(lps, row, col):
@@ -45,8 +46,9 @@ def largest_bybase(lps, row, col):
     temp = deepcopy(lps)
     for size in range(1, lps[row][col] + 1):
         zero_out(temp, row, col, size)
-        tlps = [[largest_plus(temp, r, c) for c in range(len(temp[r]))] for r in range(len(temp))]
-        largest = max(largest, stoa(size) * stoa(max(chain.from_iterable(tlps))))
+        tlp = max(largest_plus(temp, r, c)
+                  for r, c in product(range(len(temp)), range(len(temp[0]))))
+        largest = max(largest, stoa(size) * stoa(tlp))
     return largest
 
 

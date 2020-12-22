47c2af572109fa3eb2d0c14f88e067f1d4699aab
SamProkopchuk <ssprokopchuk@gmail.com>
2020-12-22 12:45:22 -0800

    Improve 3d-surface-area solution.py

--- hackerrank/3d-surface-area/solution.py
+++ hackerrank/3d-surface-area/solution.py
@@ -1,12 +1,12 @@
+from itertools import product
+
 h, w = map(int, input().split())
-board = []
+board = [list(map(int, input().strip().split())) for _ in range(h)]
 sa = 2 * h * w
-for row in range(h):
-    board.append(list(map(int, input().strip().split())))
-    for col in range(w):
-        n = board[row][col]
-        sa += n if row == 0 else abs(n - board[row - 1][col])
-        sa += n if col == 0 else abs(n - board[row][col - 1])
-        if row == h-1: sa += n
-        if col == w-1: sa += n
+for row, col in product(range(h), range(w)):
+    n = board[row][col]
+    sa += n if row == 0 else abs(n - board[row - 1][col])
+    sa += n if col == 0 else abs(n - board[row][col - 1])
+    if row == h - 1: sa += n
+    if col == w - 1: sa += n
 print(sa)

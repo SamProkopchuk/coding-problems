68a077cf90a5f9ad429ea8ca92f478a15afed87d
SamProkopchuk <ssprokopchuk@gmail.com>
2020-12-23 01:40:28 -0800

    Simplify 3d-surface-area cpp solution

--- hackerrank/3d-surface-area/solution.cpp
+++ hackerrank/3d-surface-area/solution.cpp
@@ -6,16 +6,13 @@ using namespace std;
 int main() {
 	int h, w, sa, v;
 	cin >> h >> w;
-	int* matrix = new int[h * w];
-	for (int idx=0; idx<h*w; idx++) {
-		cin >> matrix[idx];
-	}
 	sa = 2 * h * w;
+	int matrix[h][w];
 	for (int r=0; r<h; r++) {
 		for (int c=0; c<w; c++) {
-			v = matrix[r * w + c];
-			sa += r ? abs(v - matrix[(r-1) * w + c]) : v;
-			sa += c ? abs(v - matrix[r * w + c - 1]) : v;
+			cin >> v, matrix[r][c] = v;
+			sa += r ? abs(v - matrix[r-1][c]) : v;
+			sa += c ? abs(v - matrix[r][c-1]) : v;
 			if (r == h-1) sa += v;
 			if (c == w-1) sa += v;
 		}

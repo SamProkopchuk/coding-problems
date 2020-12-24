8f4b8b53b4b445177f791509d9b278d35cd2f86c
SamProkopchuk <ssprokopchuk@gmail.com>
2020-12-23 23:20:58 -0800

    Format cpp solutions

--- hackerrank/3d-surface-area/solution.cpp
+++ hackerrank/3d-surface-area/solution.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
-#include<cmath>
+#include <cmath>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-	int h, w, sa, v;
-	cin >> h >> w;
-	sa = 2 * h * w;
-	int matrix[h][w];
-	for (int r=0; r<h; r++) {
-		for (int c=0; c<w; c++) {
-			cin >> v, matrix[r][c] = v;
-			sa += r ? abs(v - matrix[r-1][c]) : v;
-			sa += c ? abs(v - matrix[r][c-1]) : v;
-			if (r == h-1) sa += v;
-			if (c == w-1) sa += v;
-		}
-	}
-	cout << sa << endl;
-	return 0;
+  int h, w, sa, v;
+  cin >> h >> w;
+  sa = 2 * h * w;
+  int matrix[h][w];
+  for (int r = 0; r < h; r++) {
+    for (int c = 0; c < w; c++) {
+      cin >> v, matrix[r][c] = v;
+      sa += r ? abs(v - matrix[r - 1][c]) : v;
+      sa += c ? abs(v - matrix[r][c - 1]) : v;
+      if (r == h - 1) sa += v;
+      if (c == w - 1) sa += v;
+    }
+  }
+  cout << sa << endl;
+  return 0;
 }

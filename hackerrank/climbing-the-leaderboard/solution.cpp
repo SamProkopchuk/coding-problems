5f566af208b1f97c3c2e272fa1fc24412073e317
SamProkopchuk <ssprokopchuk@gmail.com>
2020-12-23 23:20:20 -0800

    Format cpp solutions

--- hackerrank/climbing-the-leaderboard/solution.cpp
+++ hackerrank/climbing-the-leaderboard/solution.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 
 int main() {
-	int n, score;
+  int n, score;
 
-	cin >> n;
-	vector<int> leaderboard;
-	for (int i=0; i<n; i++) {
-		cin >> score;
-		if (leaderboard.empty() || score != leaderboard.back())
-			leaderboard.push_back(score);
-	} // leaderboard now has no duplicates.
+  cin >> n;
+  vector<int> leaderboard;
+  for (int i = 0; i < n; i++) {
+    cin >> score;
+    if (leaderboard.empty() || score != leaderboard.back())
+      leaderboard.push_back(score);
+  }  // leaderboard now has no duplicates.
 
-	cin >> n;
-	vector<int> alice_scores(n);
-	for (int idx=0; idx<n; idx++) {
-		cin >> alice_scores[idx];
-	}
+  cin >> n;
+  vector<int> alice_scores(n);
+  for (int idx = 0; idx < n; idx++) {
+    cin >> alice_scores[idx];
+  }
 
-	for (int ascore : alice_scores) {
-		while (leaderboard.size() && leaderboard.back() <= ascore) {
-			leaderboard.pop_back();
-		}
-		cout << leaderboard.size() + 1 << endl;
-	}
-	return 0;
+  for (int ascore : alice_scores) {
+    while (leaderboard.size() && leaderboard.back() <= ascore) {
+      leaderboard.pop_back();
+    }
+    cout << leaderboard.size() + 1 << endl;
+  }
+  return 0;
 }

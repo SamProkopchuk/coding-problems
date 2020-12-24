cb930f88abbf6466abec083d11364b1c5dd87e5c
SamProkopchuk <ssprokopchuk@gmail.com>
2020-12-23 23:10:33 -0800

    Add bigger-is-greater cpp solution

---
+++ hackerrank/bigger-is-greater/solution.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int test, tests, idx, swapidx;
+  string s;
+  cin >> tests;
+  for (test = 0; test < tests; test++) {
+    s.clear();
+    cin >> s;
+    for (idx = s.length() - 1; idx > -1; idx--) {
+      if (idx != s.length() - 1 && s[idx] < s[idx + 1]) {
+        swap(s[idx], *min_element(next(s.begin(), idx + 1), s.end(),
+                                  [&s, &idx](char a, char b) {
+                                    return (a > s[idx]) && (a < b);
+                                  }));
+        sort(next(s.begin(), idx + 1), s.end());
+        cout << s << endl;
+        break;
+      }
+    }
+    if (idx == -1) cout << "no answer" << endl;
+  }
+  return 0;
+}

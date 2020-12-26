e97bd8babf4d60b45fdcc85551c407d069a38b47
SamProkopchuk <ssprokopchuk@gmail.com>
2020-12-26 13:32:53 -0800

    Format

--- hackerrank/non-divisible-subset/solution.cpp
+++ hackerrank/non-divisible-subset/solution.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
-#include<unordered_map>
+#include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 int main() {
-	int n, mod, temp, res;
-	cin >> n >> mod;
-	unordered_map<int, int> remcount;
-	for (int posrem = 0; posrem < mod; posrem++) {
-		// Simple defaultdict-like behavior
-		remcount[posrem] = 0;
-	}
-	for (int c=0; c<n; c++) {
-		cin >> temp;
-		remcount[temp % mod] += 1;
-	}
-	res = (remcount[0] > 0) + (mod % 2 == 0 && remcount[mod / 2]);
-	for (int rem=1; rem<(mod+1)/2; rem++) {
-		res += max(remcount[rem], remcount[mod-rem]);
-	}
-	cout <<  res << endl;
-	return 0;
+  int n, mod, temp, res;
+  cin >> n >> mod;
+  unordered_map<int, int> remcount;
+  for (int posrem = 0; posrem < mod; posrem++) {
+    // Simple defaultdict-like behavior
+    remcount[posrem] = 0;
+  }
+  for (int c = 0; c < n; c++) {
+    cin >> temp;
+    remcount[temp % mod] += 1;
+  }
+  res = (remcount[0] > 0) + (mod % 2 == 0 && remcount[mod / 2]);
+  for (int rem = 1; rem < (mod + 1) / 2; rem++) {
+    res += max(remcount[rem], remcount[mod - rem]);
+  }
+  cout << res << endl;
+  return 0;
 }

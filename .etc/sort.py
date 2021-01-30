16d8210bb60f23c5e27d7d776b37ba9beff823a0
SamProkopchuk <ssprokopchuk@gmail.com>
2021-01-29 17:31:58 -0800

    Add binary insertion sort

--- .etc/sort.py
+++ .etc/sort.py
@@ -1,15 +1,28 @@
 from random import randint, shuffle
 
 
+def binary_insertion_sort(lst: list) -> None:
+    def binary_insert(r: int, x: int) -> None:
+        l = 0
+        mid = (l + r + 1) // 2
+        while l <= r:
+            if x < lst[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+            mid = (l + r + 1) // 2
+        lst.insert(mid, x)
+    for i in range(len(lst)):
+        binary_insert(i-1, lst.pop(i))
+
+
 def qsort(lst: list, l=None, r=None) -> None:
-    if not lst:
-        return
     if l is None:
         l = 0
     if r is None:
         r = len(lst) - 1
 
-    def part(l: int, r: int):
+    def part(l: int, r: int) -> int:
         p, r_ = l, r
         while l < r:
             while l < r_ and lst[p] >= lst[l]:
@@ -27,9 +40,9 @@ def qsort(lst: list, l=None, r=None) -> None:
         qsort(lst, r_ + 1, r)
 
 
-def visual_test(lst: list) -> None:
+def visual_test(sort_fn, lst: list) -> None:
     print(lst, end=' => ')
-    qsort(lst)
+    sort_fn(lst)
     print(lst)
 
 
@@ -38,12 +51,12 @@ def main():
     for _ in range(5):
         lst = [*range(10)]
         shuffle(lst)
-        visual_test(lst)
+        visual_test(qsort, lst)
 
     print(f'{"10 Random Integers [0, 10)":-^{40}}')
     for _ in range(10):
         lst = [randint(0, 9) for _ in range(10)]
-        visual_test(lst)
+        visual_test(binary_insertion_sort, lst)
 
 
 if __name__ == '__main__':

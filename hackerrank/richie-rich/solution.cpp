128bf5af079b7092fe474cc88306db44717da776
SamProkopchuk <ssprokopchuk@gmail.com>
2021-02-27 02:00:52 -0800

    Make code even more readable

--- hackerrank/richie-rich/solution.cpp
+++ hackerrank/richie-rich/solution.cpp
@@ -11,20 +11,14 @@ int highest_value_palindrome(string &s, size_t k) {
   if (k < d) return -1;
   for (i = 0; i < l / 2; ++i) {
     if (s[i] != s[l - 1 - i]) {
-      (s[i] > s[l - 1 - i]) ? (s[l - 1 - i] = s[i]) : (s[i] = s[l - 1 - i]);
+      s[i] = s[l - 1 - i] = max(s[i], s[l - 1 - i]);
       if (k == d || s[i] == '9')
-        --k, --d;
-      else if (k >= 2) {
-        s[i] = '9';
-        s[l - 1 - i] = '9';
-        k -= 2;
-        --d;
-      }
-    } else if (k > d && k >= 2 && s[i] != '9') {
-      s[i] = '9';
-      s[l - 1 - i] = '9';
-      k -= 2;
-    }
+        --k;
+      else if (k >= 2)
+        s[i] = s[l - 1 - i] = '9', k -= 2;
+      --d;
+    } else if (k > d && k >= 2 && s[i] != '9')
+      s[i] = s[l - 1 - i] = '9', k -= 2;
   }
   if (l % 2 && k > 0) s[l / 2] = '9';
   return 0;

00cd25060bf7a49a3c75d86a2c1665f309632a32
Sam Prokopchuk <ssprokopchuk@gmail.com>
2021-03-19 22:43:31 -0400

    O(1) space complexity solution

--- hackerrank/maxsubarray/solution.cpp
+++ hackerrank/maxsubarray/solution.cpp
@@ -11,30 +11,21 @@ typedef signed long long ll;
 using namespace std;
 
 int main() {
-  int t, n, i, maxsubarr, maxsubseq, largest;
+  int t, n, i, x, cursum, maxsubarr, maxsubseq, largest;
   cin >> t;
   while (t--) {
     cin >> n;
-    vector<int> arr(n);
-    vector<int> cumsum(n);
-    vector<int> revmaxcumsum(n);
-    maxsubseq = 0;
+    cursum = maxsubarr = maxsubseq = 0;
     largest = INT_MIN;
     FOR(i, n) {
-      cin >> arr[i];
-      maxsubseq = max(maxsubseq, maxsubseq + arr[i]);
-      largest = max(largest, arr[i]);
+      cin >> x;
+      largest = max(largest, x);
+      cursum += x;
+      maxsubseq = max(maxsubseq, maxsubseq + x);
+      maxsubarr = max(maxsubarr, cursum);
+      cursum = max(cursum, 0);
     }
-    if (largest < 0) maxsubseq = largest;
-    cumsum[0] = arr[0];
-    for (i = 1; i < n; ++i) cumsum[i] = cumsum[i - 1] + arr[i];
-    revmaxcumsum[n - 1] = cumsum[n - 1];
-    for (i = n - 2; i >= 0; --i)
-      revmaxcumsum[i] = max(revmaxcumsum[i + 1], cumsum[i]);
-    maxsubarr = revmaxcumsum[0];
-    FOR(i, n - 1) {
-      maxsubarr = max(maxsubarr, revmaxcumsum[i + 1] - cumsum[i]);
-    }
+    if (largest < 0) maxsubseq = maxsubarr = largest;
     cout << maxsubarr << " " << maxsubseq << endl;
   }
   return 0;

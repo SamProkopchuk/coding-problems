e7766a88ff5f1ef370738e7d493bd15ba78a7935
Sam Prokopchuk <ssprokopchuk@gmail.com>
2021-03-27 14:25:30 -0400

    Clearer map names

--- hackerrank/longest-increasing-subsequence/solution.cpp
+++ hackerrank/longest-increasing-subsequence/solution.cpp
@@ -12,24 +12,24 @@ int main() {
   ios_base::sync_with_stdio(false);
   int n, i, x;
   map<int, int> lis2MinTail = {{1, INT_MAX}};
-  map<int, int> minTail = {{-INT_MAX, 1}};
+  map<int, int> minTail2Lis = {{-INT_MAX, 1}};
   std::map<int, int>::iterator it;
   cin >> n;
   FOR(i, n) {
     cin >> x;
     if (x < lis2MinTail[1]) {
-      minTail.erase(-lis2MinTail[1]);
+      minTail2Lis.erase(-lis2MinTail[1]);
       lis2MinTail[1] = x;
-      minTail[-x] = 1;
-    } else if ((it = minTail.upper_bound(-x)) != minTail.end()) {
+      minTail2Lis[-x] = 1;
+    } else if ((it = minTail2Lis.upper_bound(-x)) != minTail2Lis.end()) {
       int lis = it->second + 1;
       if (!lis2MinTail.count(lis)) {
         lis2MinTail[lis] = x;
-        minTail[-x] = lis;
+        minTail2Lis[-x] = lis;
       } else if (x < lis2MinTail[lis]) {
-        minTail.erase(-lis2MinTail[lis]);
+        minTail2Lis.erase(-lis2MinTail[lis]);
         lis2MinTail[lis] = x;
-        minTail[-x] = lis;
+        minTail2Lis[-x] = lis;
       }
     }
   }

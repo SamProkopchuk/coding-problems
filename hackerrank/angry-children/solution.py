d8712fce2d3e347fb731cac9ae5cf2a330d5c129
Sam Prokopchuk <ssprokopchuk@gmail.com>
2021-04-16 06:11:56 -0400

    Simplify code

--- hackerrank/angry-children/solution.py
+++ hackerrank/angry-children/solution.py
@@ -1,5 +1,3 @@
 n, k = int(input()), int(input()) - 1
-arr = [int(input()) for _ in range(n)]
-arr.sort()
-min_ = min(arr[i+k]-arr[i] for i in range(n-k))
-print(min_)
+arr = sorted(int(input()) for _ in range(n))
+print(min(arr[i+k]-arr[i] for i in range(n-k)))

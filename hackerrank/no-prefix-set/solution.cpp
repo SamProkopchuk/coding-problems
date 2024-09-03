640c995d21aaaedcd0672ad63f40b980c989ee4a
Sam Prokopchuk <ssprokopchuk@gmail.com>
2024-09-03 16:35:34 -0700

    Add no-prefix-set solution

---
+++ hackerrank/no-prefix-set/solution.cpp
@@ -0,0 +1,52 @@
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace {
+
+constexpr int AL_SIZE = 'j' - 'a' + 1;
+constexpr int Ord(char c) { return c - 'a'; }
+
+void solve(const vector<string> &words) {
+  vector<map<char, int>> trie(1);
+  for (const auto &word : words) {
+    int curr_node_idx = 0;
+    bool common_prefix = true;
+    for (int i = 0; i < word.size(); ++i) {
+      const char &c = word[i];
+      const auto &it = trie[curr_node_idx].find(c);
+      if ((i == word.size() - 1 && it != trie[curr_node_idx].end()) ||
+          (i != 0 && common_prefix && trie[curr_node_idx].size() == 0)) {
+        // Current word is prefix or prev one, or prev one is prefix of current.
+        cout << "BAD SET" << endl;
+        cout << word << endl;
+        return;
+      }
+      if (it != trie[curr_node_idx].end()) {
+        curr_node_idx = it->second;
+        continue;
+      }
+      trie[curr_node_idx][c] = trie.size();
+      curr_node_idx = trie.size();
+      trie.push_back({});
+      common_prefix = false;
+    }
+  }
+  cout << "GOOD SET" << endl;
+}
+
+} // namespace
+
+int main() {
+  int n;
+  cin >> n;
+  vector<string> words(n);
+  for (auto &word : words) {
+    cin >> word;
+  }
+  solve(words);
+}

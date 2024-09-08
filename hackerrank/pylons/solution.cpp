ba73b6c4e615e893f8368f5074c4b3876c45aaac
Sam Prokopchuk <ssprokopchuk@gmail.com>
2024-09-08 15:17:18 -0700

    Slightly simpler

--- hackerrank/pylons/solution.cpp
+++ hackerrank/pylons/solution.cpp
@@ -12,20 +12,19 @@ namespace {
 constexpr int MAX_N = 100000;
 
 int get_solution(const bitset<MAX_N> &b, int n, int k) {
-  int last = -1;
   int num_plants = 0;
-  for (int i = 0; i < n; ++i) {
-    if (last == -1 || i - last >= k) {
-      for (int j = min(i + k - 1, n - 1); j >= max(0, i - k + 1); --j) {
-        if (b[j]) {
-          last = j;
-          ++num_plants;
-          break;
-        }
+  for (int i = 0; i < n;) {
+    bool found = false;
+    for (int j = min(i + k - 1, n - 1); j >= max(0, i - k + 1); --j) {
+      if (b[j]) {
+        i = j + k;
+        ++num_plants;
+        found = true;
+        break;
       }
-      if (last == -1 || i - last >= k) {
-        return -1;
-      }
+    }
+    if (!found) {
+      return -1;
     }
   }
   return num_plants;

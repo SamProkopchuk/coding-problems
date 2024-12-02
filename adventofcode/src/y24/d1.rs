58229acd2b6dd774cbba76e3cb2b98435c9bebb1
Sam Prokopchuk <ssprokopchuk@gmail.com>
2024-12-01 21:02:18 -0800

    Add day 1 p2

--- adventofcode/src/y24/d1.rs
+++ adventofcode/src/y24/d1.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::iter::zip;
+
 use crate::adventofcode::AdventOfCode;
 
 pub struct Day;
@@ -7,10 +10,10 @@ impl AdventOfCode for Day {
         let (mut ls, mut rs): (Vec<i32>, Vec<i32>) = Day::input_lines(2024, 1)?
             .into_iter()
             .map(|line| {
-                let (first, second) = line.split_once("   ").unwrap();
+                let (l, r) = line.split_once("   ").unwrap();
                 (
-                    first.parse::<i32>().ok().unwrap(),
-                    second.parse::<i32>().ok().unwrap(),
+                    l.parse::<i32>().ok().unwrap(),
+                    r.parse::<i32>().ok().unwrap(),
                 )
             })
             .unzip();
@@ -18,9 +21,17 @@ impl AdventOfCode for Day {
         rs.sort();
 
         // Assume no overflow
-        let distance: i32 =
-            std::iter::zip(ls.iter(), rs.iter()).fold(0, |acc, (l, r)| acc + (l - r).abs());
+        let distance: i32 = zip(ls.iter(), rs.iter()).fold(0, |acc, (l, r)| acc + (l - r).abs());
         println!("{distance}");
+
+        let rcounts = rs.iter().fold(HashMap::new(), |mut acc, r| {
+            acc.entry(r).and_modify(|c| *c += 1).or_insert(1);
+            acc
+        });
+        let similarity: i32 = ls
+            .iter()
+            .fold(0, |acc, l| acc + l * rcounts.get(l).unwrap_or(&0));
+        println!("{similarity}");
 
         Ok(())
     }

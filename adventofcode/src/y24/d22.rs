480db8ead971c1992863aeb01f127f28501dff2e
Sam Prokopchuk <ssprokopchuk@gmail.com>
2025-01-01 00:45:56 -0800

    Update d22.rs

--- adventofcode/src/y24/d22.rs
+++ adventofcode/src/y24/d22.rs
@@ -9,11 +9,8 @@ type Number = u64;
 const PRUNE: Number = 16777216;
 
 fn next_number(n: Number) -> Number {
-    // Step 1:
     let n: Number = (n ^ (n * 64)) % PRUNE;
-    // Step 2:
     let n: Number = (n ^ (n / 32)) % PRUNE;
-    // Step 3:
     let n: Number = (n ^ (n * 2048)) % PRUNE;
     n
 }
@@ -25,31 +22,29 @@ impl AdventOfCode for Day {
             .map(|line| line.parse::<Number>().unwrap())
             .collect();
         let mut next_day_secret_numbers_sum: Number = 0;
-        let mut last_numbers: (Number, Number, Number, Number, Number) = (0, 0, 0, 0, 0);
-        let mut last_digits: (i8, i8, i8, i8, i8) = (0, 0, 0, 0, 0);
-        let mut last_deltas: (i8, i8, i8, i8) = (0, 0, 0, 0);
+        let mut last_numbers: [Number; 5] = [0, 0, 0, 0, 0];
+        let mut last_digits: [i8; 5] = [0, 0, 0, 0, 0];
+        let mut last_deltas: [i8; 4] = [0, 0, 0, 0];
         let mut total_bananas_by_sequence: HashMap<(i8, i8, i8, i8), Number> = HashMap::new();
         let mut bananas_by_sequence: HashMap<(i8, i8, i8, i8), Number> = HashMap::new();
         for initial_secret_number in initial_secret_numbers {
-            last_numbers.4 = initial_secret_number;
-            last_digits.4 = (last_numbers.4 % 10) as i8;
+            last_numbers[0] = initial_secret_number;
+            last_digits[0] = (initial_secret_number % 10) as i8;
             for days_gone_by in 1..=2000 {
-                last_numbers.0 = last_numbers.1;
-                last_numbers.1 = last_numbers.2;
-                last_numbers.2 = last_numbers.3;
-                last_numbers.3 = last_numbers.4;
-                last_numbers.4 = next_number(last_numbers.4);
-                last_digits.0 = last_digits.1;
-                last_digits.1 = last_digits.2;
-                last_digits.2 = last_digits.3;
-                last_digits.3 = last_digits.4;
-                last_digits.4 = (last_numbers.4 % 10) as i8;
-                last_deltas.0 = last_deltas.1;
-                last_deltas.1 = last_deltas.2;
-                last_deltas.2 = last_deltas.3;
-                last_deltas.3 = last_digits.4 - last_digits.3;
+                last_numbers.rotate_right(1);
+                last_numbers[0] = next_number(last_numbers[1]);
+                last_digits.rotate_right(1);
+                last_digits[0] = (last_numbers[0] % 10) as i8;
+                last_deltas.rotate_right(1);
+                last_deltas[0] = last_digits[0] - last_digits[1];
                 if days_gone_by >= 4 {
-                    let bananas: Number = last_digits.4 as Number;
+                    let bananas: Number = last_digits[0] as Number;
+                    let last_deltas: (i8, i8, i8, i8) = (
+                        last_deltas[0],
+                        last_deltas[1],
+                        last_deltas[2],
+                        last_deltas[3],
+                    );
                     if !bananas_by_sequence.contains_key(&last_deltas) {
                         bananas_by_sequence.insert(last_deltas, bananas);
                     }
@@ -61,7 +56,7 @@ impl AdventOfCode for Day {
                     .and_modify(|b| *b += bananas)
                     .or_insert(bananas);
             }
-            next_day_secret_numbers_sum += last_numbers.4;
+            next_day_secret_numbers_sum += last_numbers[0];
         }
         println!("{}", next_day_secret_numbers_sum);
         println!("{}", total_bananas_by_sequence.values().max().unwrap());
